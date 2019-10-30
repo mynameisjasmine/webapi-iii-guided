c8f915f2b6ab6a309c0214a3c3bcf2ee316b378d
Jasmine <jasminecharles@hotmail.com>
2019-10-30 10:32:18 -0700

    refactored gateKeeper

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet'); // importing helmet security middlware
+const morgan = require('morgan')
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
@@ -16,11 +17,29 @@ const loggerMethod = (req, res, next) => {
   next()
 }
 
+const gateKeeper = (req, res, next) => {
+  const password = req.headers.password;
+  
+  if(password === '') {
+    res.status(400).json({message: 'please provide a password'})
+  }
+  else if(password.toLowerCase() === 'mellon') {
+    next();
+
+  } else {
+    res.status(401).json({you: 'cannot pass!!'})
+  
+ }
+}
+
 //global middleware
+
 server.use(helmet()) //third-party middleware
 server.use(express.json());
+server.use(gateKeeper);
 server.use(dateLogger) //custom middleware
 server.use(loggerMethod) //custom middleware
+server.use(morgan('dev'));
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
